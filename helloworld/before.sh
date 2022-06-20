26b32fc6b2b8322dd4b635a04e9912e1abc2142b
ron-ait <100356208+ron-ait@users.noreply.github.com>
2022-06-20 15:44:14 +0530

    Create before.sh

---
+++ helloworld/before.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+sudo su 
+if [ -d /var/www/html/build ] 
+then
+    rm -rf /var/www/html/build
+fi
+
+mv reactjs /etc/nginx/sites-available/
+if [ $? -eq 0 ] 
+then
+   cd /etc/nginx/sites-enabled/
+
+if [ $? -eq 0 ]
+then
+    ln -s /etc/nginx/sites-available/reactjs .
+fi
+fi
